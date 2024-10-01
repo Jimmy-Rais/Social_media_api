72f9b717589d28cdec11851c50b74d7b08a83cdd
Jimmy-Rais <raisgachaba@gmail.com>
2024-10-01 20:36:39 +0530

    Env variables config

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,11 +1,11 @@
 from fastapi import Depends,status,HTTPException
 from jose import JWTError,jwt
 from datetime import datetime,timedelta, timezone
-from . import schemas
+from . import schemas,config
 from fastapi.security import OAuth2PasswordBearer
 Oauth2_scheme=OAuth2PasswordBearer(tokenUrl="token")
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+SECRET_KEY=config.settings.secret_key
+ALGORITHM =config.settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 def create_access_token(data:dict):
     to_encode=data.copy()
